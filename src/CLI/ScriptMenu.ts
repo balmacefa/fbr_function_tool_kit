fd52138dbb14505a53039d3ab9f361dc8193954f
Fabián Balmaceda <fabianbalmaceda2@gmail.com>
2023-12-05 15:25:02 -0600

    menu

--- src/CLI/ScriptMenu.ts
+++ src/CLI/ScriptMenu.ts
@@ -3,79 +3,88 @@ import inquirer from 'inquirer';
 import { MainUtils } from '../main_utils';
 
 // TODO: crea a go back submenu and exit options
+
 interface PackageJson {
     scripts: Record<string, string>;
 }
 
-function readPackageJson(): PackageJson {
-    const data = MainUtils.read_file_from_root('package.json').fileContent;
-    return JSON.parse(data);
-}
+class ScriptExecutor {
+    private packageJson: PackageJson;
 
-async function executeScript(scriptName: string, scriptCommand: string) {
-    try {
-        console.log('-------------------------------------*');
-        console.log(`Executing package.json script: "${scriptName}": "${scriptCommand}"`);
-        console.log('-------------------------------------*');
-        console.log('**-------------------------------------**');
-        const result = await execa('pnpm', [scriptName]);
-        console.log(result.stdout);
-        console.log('**-------------------------------------**');
-    } catch (error) {
-        console.error(`Error executing script: ${scriptName}\n`, error);
+    constructor() {
+        this.packageJson = this.readPackageJson();
     }
-}
 
-function categorizeScripts(scripts: PackageJson['scripts']) {
-    const categories: any = {};
+    private readPackageJson(): PackageJson {
+        const data = MainUtils.read_file_from_root('package.json').fileContent;
+        return JSON.parse(data);
+    }
 
-    Object.entries(scripts).forEach(([key, value]) => {
-        const [category, scriptName] = key.split(':');
-        if (!categories[category]) {
-            categories[category] = [];
+    private async executeScript(scriptName: string, scriptCommand: string) {
+        try {
+            console.log('-------------------------------------*');
+            console.log(`Executing package.json script: "${scriptName}": "${scriptCommand}"`);
+            console.log('-------------------------------------*');
+            console.log('**-------------------------------------**');
+            const result = await execa('pnpm', [scriptName]);
+            console.log(result.stdout);
+            console.log('**-------------------------------------**');
+        } catch (error) {
+            console.error(`Error executing script: ${scriptName}\n`, error);
         }
-        categories[category].push({ name: scriptName || key, value: key, script: value });
-    });
+    }
 
-    return categories;
-}
+    private categorizeScripts(scripts: PackageJson['scripts']) {
+        const categories: any = {};
 
-async function createScriptsMenu(scripts: PackageJson['scripts']) {
-    const categories = categorizeScripts(scripts);
+        Object.entries(scripts).forEach(([key, value]) => {
+            const [category, scriptName] = key.split(':');
+            if (!categories[category]) {
+                categories[category] = [];
+            }
+            categories[category].push({ name: scriptName || key, value: key, script: value });
+        });
 
-    const categoryChoices = Object.keys(categories).map(category => ({
-        name: category,
-        value: category
-    }));
+        return categories;
+    }
 
-    const categoryAnswer = await inquirer.prompt([
-        {
-            type: 'list',
-            name: 'selectedCategory',
-            message: 'Select a category:',
-            choices: categoryChoices
+    public async createScriptsMenu() {
+        const categories = this.categorizeScripts(this.packageJson.scripts);
+
+        const categoryChoices = Object.keys(categories).map(category => ({
+            name: category,
+            value: category
+        }));
+
+        const categoryAnswer = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'selectedCategory',
+                message: 'Select a category:',
+                choices: categoryChoices
+            }
+        ]);
+
+        const selectedCategory = categoryAnswer.selectedCategory;
+        const scriptChoices = categories[selectedCategory];
+
+        const scriptAnswer = await inquirer.prompt([
+            {
+                type: 'list',
+                name: 'selectedScript',
+                message: `Select a script from ${selectedCategory}:`,
+                choices: scriptChoices
+            }
+        ]);
+
+        const selectedScript = scriptAnswer.selectedScript;
+        if (selectedScript && this.packageJson.scripts[selectedScript]) {
+            await this.executeScript(selectedScript, this.packageJson.scripts[selectedScript]);
         }
-    ]);
-
-    const selectedCategory = categoryAnswer.selectedCategory;
-    const scriptChoices = categories[selectedCategory];
-
-    const scriptAnswer = await inquirer.prompt([
-        {
-            type: 'list',
-            name: 'selectedScript',
-            message: `Select a script from ${selectedCategory}:`,
-            choices: scriptChoices
-        }
-    ]);
-
-    const selectedScript = scriptAnswer.selectedScript;
-    if (selectedScript && scripts[selectedScript]) {
-        await executeScript(selectedScript, scripts[selectedScript]);
     }
 }
 
 (async () => {
-    const packageJson = readPackageJson();
-    await createScriptsMenu(packageJson.scripts);
+    const scriptExecutor = new ScriptExecutor();
+    await scriptExecutor.createScriptsMenu();
 })();
